d16c8b92f0145239676fedfa5c3c53f324a24d98
axv50 <alexvega50@gmail.com>
2021-10-20 18:22:33 -0500

    try

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 
 function Header() {
   return (
     <div className="Header">
-      <h2>
-        <Link to="/">Alexis Vega</Link>
-      </h2>
-      <Link to="/projects">Projects</Link>
+      
+     
     </div>
   );
 }
